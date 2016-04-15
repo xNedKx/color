3acd209b783bd99b7d9990b4e108e1b687919408
xNedKx <xnedkx@gmail.com>
2016-04-15 23:47:43 +0800

    skip gray and use requestAnimationFrame

--- grayscale_worker_cc.js
+++ grayscale_worker_cc.js
@@ -6,14 +6,16 @@ function hToLeft(e){
         for(var j = 0; j < img.width; j++){
             var pos = (i * img.width + j) * 4,
                 key = img.data.slice(pos,pos+4).join("|")
-            if( !cache.hasOwnProperty(key) ){
-                color[0] = img.data[pos]
-                color[1] = img.data[pos+1]
-                color[2] = img.data[pos+2]
-                color.alphaValue = img.data[pos+3]
-                cache[key] = color[m](false).grayscale().css()
+            if( !(img.data[pos] == img.data[pos+1] && img.data[pos+1] == img.data[pos+2]) || (i == img.height-1 && j == img.width-1)){
+                if( !cache.hasOwnProperty(key) ){
+                    color[0] = img.data[pos]
+                    color[1] = img.data[pos+1]
+                    color[2] = img.data[pos+2]
+                    color.alphaValue = img.data[pos+3]
+                    cache[key] = color[m](false).grayscale()[0]
+                }
+                postMessage({color: cache[key], x: j, y: i})
             }
-            postMessage({color: cache[key], x: j, y: i})
         }
     }
 }
